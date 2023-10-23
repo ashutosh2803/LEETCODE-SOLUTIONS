ded12bf0cd5ac32c5e7d58dde8e2b59cc2ceda60
Ashutosh Kumar <40117155+ashutosh2803@users.noreply.github.com>
2023-10-24 00:57:24 +0530

    Time: 61 ms (50.20%), Space: 44 MB (9.91%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.js
+++ 0342-power-of-four/0342-power-of-four.js
@@ -3,8 +3,6 @@
  * @return {boolean}
  */
 var isPowerOfFour = function(n) {
-    while(n % 4 == 0 && n >= 4){
-        n = n / 4;
-    }
+    while(n % 4 == 0 && n >= 4) n = n / 4;
     return n == 1;
 };
