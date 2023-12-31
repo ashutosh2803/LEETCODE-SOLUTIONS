46693d336850027fffd16ac744bc4aeab3336535
Ashutosh Kumar <40117155+ashutosh2803@users.noreply.github.com>
2023-12-31 18:53:10 +0530

    Time: 110 ms (8.89%), Space: 41.6 MB (90.00%) - LeetHub

---
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxLengthBetweenEqualCharacters = function(s) {
+    max = -1
+
+    for (let i = 0; i < s.length; i++) {
+        len = i - s.indexOf(s[i]) - 1
+        if (len > max)
+            max = len
+    }
+
+    return max
+
+};
