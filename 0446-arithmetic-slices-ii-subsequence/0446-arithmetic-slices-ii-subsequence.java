280120a5830a7d2731a69e987b3b03a53b22c421
Ashutosh Kumar <40117155+ashutosh2803@users.noreply.github.com>
2024-01-08 02:15:20 +0530

    Time: 147 ms (68.89%), Space: 90.7 MB (50.00%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+       int result = 0;
+       //Initiliaze an array of HashMap
+       HashMap<Integer,Integer>[]maps= new HashMap[nums.length];
+       
+       //Iterate through each element of the array
+       for(int i=0;i<nums.length;i++){
+       maps[i]=new HashMap<>();
+       int num=nums[i];
+       for(int j=0;j<i;j++){
+           if((long)num-nums[j]>Integer.MAX_VALUE)continue;
+           if((long)num-nums[j]<Integer.MIN_VALUE)continue;
+           int diff = num-nums[j];
+
+      //Update count for the current diff in the HashMap for the current element A[i]
+        int count =maps[j].getOrDefault(diff,0);
+        maps[i].put(diff,maps[i].getOrDefault(diff,0)+(count+1));
+        result+=count;
+        }
+       }
+       return result;
+    }
+}
