4e502a232b874832561719295eb57889cbfcbf11
Ashutosh Kumar <40117155+ashutosh2803@users.noreply.github.com>
2024-03-04 00:51:43 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (94.02%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,19 +10,23 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummyNode = new ListNode();
-        dummyNode.next = head;
-        ListNode fast = dummyNode;
-        ListNode slow = dummyNode;
-
-        for(int i = 1; i <= n; ++i){
-            fast = fast.next;
-        }
-        while(fast.next != null){
-            fast = fast.next;
-            slow = slow.next;
-        }
-        slow.next = slow.next.next;
-        return dummyNode.next; 
+         ListNode newHead = reverse(head);
+         if(n == 1)return reverse(newHead.next);//whether the node to be removed is last node or not
+         ListNode curr = head;
+         for(curr = newHead; curr != null && n-1 > 1; curr = curr.next){//one node previous to the node to be removed
+           n--;
+         }
+         curr.next = curr.next.next;//update the link
+         return reverse(newHead);
+    }
+    private ListNode reverse(ListNode head){
+              ListNode curr = head, prev = null;
+              while(curr != null){
+                  ListNode next = curr.next;
+                  curr.next = prev;
+                  prev = curr;
+                  curr = next;
+              }
+              return prev;
     }
 }
